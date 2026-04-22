bda2e26c438277537e1ceebe5dcd6b22fbdb3a74
eklownr <eklownr@gmail.com>
2026-04-22 23:42:34 +0200

    coffeeshop

--- vite-project/src/__tests__/coffee.test.ts
+++ vite-project/src/__tests__/coffee.test.ts
@@ -1,25 +1,80 @@
-import { createDrink } from "../coffeshop/createDrink";
+import {
+	createDrink,
+	prepareDrink,
+	processPayment,
+	orderDrink,
+} from "../coffeshop/createDrink";
 import { describe, expect, it } from "vitest";
 
-describe("test createDrink", () => {
+describe("CreateDrink", () => {
 	it("test createDrink Cappuccino", () => {
-		expect(createDrink("Cappuccino")).toBe("Cappuccino - 3");
+		expect(createDrink("Cappuccino")).toStrictEqual({
+			name: "Cappuccino",
+			price: "3",
+		});
 	});
 	it("test createDrink Latte", () => {
-		expect(createDrink("Latte")).toBe("Latte - 2");
+		expect(createDrink("Latte")).toStrictEqual({
+			name: "Latte",
+			price: "4",
+		});
 	});
 	it("test createDrink Americano", () => {
-		expect(createDrink("Americano")).toBe("Americano - 2");
+		expect(createDrink("Americano")).toStrictEqual({
+			name: "Americano",
+			price: "2",
+		});
 	});
 	it("test createDrink Mocha", () => {
-		expect(createDrink("Mocha")).toBe("Mocha - 2");
+		expect(createDrink("Mocha")).toStrictEqual({
+			name: "Mocha",
+			price: "6",
+		});
 	});
 
 	// make it fail and fix function to return "Lat - 0"
 	it("#test createDrink to fail add Lat", () => {
-		expect(createDrink("Lat")).toBe("Lat - sold out");
+		expect(createDrink("Lat")).toStrictEqual({
+			name: "Lat",
+			price: "sold out",
+		});
 	});
 	it("#test createDrink to fail add Mjölk", () => {
-		expect(createDrink("Mjölk")).toBe("Mjölk - sold out");
+		expect(createDrink("Mjölk")).toStrictEqual({
+			name: "Mjölk",
+			price: "sold out",
+		});
+	});
+});
+
+describe("PrepareDrink ticket number", () => {
+	it("test prepareDrink Cappuccino", () => {
+		expect(prepareDrink("Cappuccino")).toBeTypeOf("number");
+	});
+	it("test prepareDrink Latte", () => {
+		expect(prepareDrink("Latte")).toBeLessThan(1000);
+	});
+	it("test prepareDrink Americano", () => {
+		expect(prepareDrink("Americano")).toBeGreaterThan(-1);
+	});
+});
+
+describe("ProcessPayment", () => {
+	it("** test processPayment", () => {
+		expect(processPayment(1, 2)).toBe(true);
+	});
+	it("** test processPayment fail", () => {
+		expect(() => processPayment(1000, 2)).toThrowError(
+			"Invalid ticket number",
+		);
+	});
+});
+
+describe("OrderDrink", () => {
+	it("test orderDrink", () => {
+		expect(() => orderDrink("Cappuccino")).not.toThrowError();
+	});
+	it("test orderDrink return true", () => {
+		expect(() => orderDrink("Latte")).toBe(true);
 	});
 });
--- vite-project/src/coffeshop/createDrink.ts
+++ vite-project/src/coffeshop/createDrink.ts
@@ -1,18 +1,43 @@
 interface DrinkType {
 	name: string;
-	price: number;
+	price: string;
 }
 
 const drinks: DrinkType[] = [
-	{ name: "Cappuccino", price: 3 },
-	{ name: "Latte", price: 2 },
-	{ name: "Americano", price: 2 },
-	{ name: "Mocha", price: 2 },
+	{ name: "Cappuccino", price: "3" },
+	{ name: "Latte", price: "4" },
+	{ name: "Americano", price: "2" },
+	{ name: "Mocha", price: "6" },
 ];
 
 // return drink type and price
-export function createDrink(type: string): string {
-	const drink = drinks.find((drink) => drink.name === type);
-	if (!drink) return `${type} - sold out`;
-	return `${drink?.name} - ${drink?.price}`;
+export function createDrink(type: string): DrinkType {
+	const drink: DrinkType = drinks.find((drink) => drink.name === type);
+	if (!drink) return { name: type, price: "sold out" };
+	return drink;
+}
+
+export function prepareDrink(drink: string): number {
+	console.log(`Preparing your ${drink}`);
+	const ticket: number = Math.floor(Math.random() * 999);
+	return ticket;
+}
+
+export function processPayment(ticket: number, amount: number): boolean {
+	if (amount <= 0) return false;
+	if (ticket <= 0 || ticket > 999) throw new Error("Invalid ticket number");
+	console.log(`Processing payment for ticket ${ticket} for ${amount}`);
+	return true;
+}
+
+export function orderDrink(drink: string): boolean {
+	// combine all functions
+	const drinkType = createDrink(drink);
+	const ticket = prepareDrink(drinkType.name);
+	processPayment(ticket, Number(drinkType.price));
+
+	console.log(
+		`Serving your ${drink} on ticket ${ticket} pric ${drinkType.price}`,
+	);
+	return true;
 }
